38874a3c769a615f023cb49c6f6e8f613dcb6586
Murad Karimov <muradkarimov07@gmail.com>
2024-10-08 23:43:52 +0300

    add cors lib and modified

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,13 @@
 const UserController = require('./user-controller');
 const PostController = require('./post-controller');
-const CommentController = require('./comment-controller');
-const LikeController = require('./like-controller');
 const FollowController = require('./follow-controller');
-
+const LikeController = require('./like-controller');
+const CommentController = require('./comment-controller');
 
 module.exports = {
   UserController,
   PostController,
-  CommentController,
+  FollowController,
   LikeController,
-  FollowController
+  CommentController
 };
